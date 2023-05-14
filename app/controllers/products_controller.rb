f39ad2a0863c0c0cad0b8f8383347a984e2f3d5a
Marco Ricaurte <marcoricaurtel@gmail.com>
2023-05-13 22:42:34 -0500

    5th commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_photo
   end
 
   def show
-    @product = Product.find(params[:id])
+    product
   end
 
   def new
@@ -22,23 +22,35 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
   def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
+    if product.update(product_params)
       redirect_to products_path, notice: 'Tu producto se ha actualizado correctamente'
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    product.destroy
+
+    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente', status: :see_other
+  end
+
+
+
+
+
+
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price)
+    params.require(:product).permit(:title, :description, :price, :photo)
   end
 
+  def product
+    @product = Product.find(params[:id])
+  end
 end
